dc9003813b9b15c2e939bccce02b56f9372b6f37
Zafar Ahasan <103955367+zafarD@users.noreply.github.com>
2024-04-04 12:59:07 +0530

    Add files via upload

---
+++ src/Form.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react"
+
+const Form = ({addColor}) => {
+
+    const [color, setColor] = useState('#7d5e97')
+    const handleSubmit = (e) => { 
+        e.preventDefault()
+        addColor(color)
+    }
+
+    return (
+        <section className="container">
+            <h4>color generator</h4>
+            <form className="color-form" onSubmit={handleSubmit}>
+                <input type="color"
+                    value={color}
+                    onChange={(e) => setColor(e.target.value)}
+                />
+                <input type="text"
+                    value={color}
+                    onChange={(e) => setColor(e.target.value)}
+                    placeholder="#7d5e97"
+                />
+                <button type="submit"
+                    className="btn"
+                    style={{ background: color}}>
+                    submit
+                </button>
+            </form>
+        </section>
+    )
+}
+export default Form;
